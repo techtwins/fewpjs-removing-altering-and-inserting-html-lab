a35ded9e491ce981400b8a732499067e684c219d
techtwins <gshevaughn@gmail.com>
2020-11-16 17:59:55 -0500

    commit tree 434d58ae65f2ff126e24c7dbb57295b3bca52db9

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector('main#main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Shevaughn is the champion'
